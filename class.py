4149604c939a7338406c0e43869754e52dfdc84a
He SUN <hehe2700@HedeMacBook-Air.local>
2026-03-12 19:27:07 +0800

    update class: multiple inheritance

--- class.py
+++ class.py
@@ -50,3 +50,115 @@ student_example1 = Student('Tom', 18)
 print(student_example1)
 student_example2 = Student('Tina', 19)
 print(str(student_example1 + student_example2))
+
+#inheritance
+class grade(Student):
+    def __init__(self, subject, score):
+        self.subject = subject
+        self.score = score
+        #However, if we only write like this. The constructor of parent class will not be called, neither will that of subclasses.
+
+#Correct method:
+class Tom_grade(Student):
+    def __init__(self, subject, score):
+        super().__init__('Tom', 18, 114514)#Have to be fixed in constructor of subclasses.
+
+#Method overriding
+
+class Animal:
+    def __init__(self):
+        pass
+    def speak():
+        print("Animal speaks")
+
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+        super().__init__()
+    def speak():
+        print("Dog speaks")
+        #If you want to use methods of parent class without modification, use: super().speak()
+
+#Abstract Base Class
+from abc import ABC, abstractmethod
+
+class shape(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def area(self):
+        pass
+
+class circle(shape):
+    def __init__(self, r):
+        super().__init__()
+        self.r = r
+    def area(self):
+        return 3.14 * (self.r ** 2)
+#One importance characteristic of ABC: The abstract methods created in superclasses have to be overridden in subclasses.
+
+#multiple inheritance
+class A:
+    def f(self):
+        print("A")
+    
+
+class B:
+    def f(self):
+        print('B')
+    pass
+
+class C(A, B):
+    pass
+
+c = C()
+c.f()#What will be printed?
+#python uses MRO(method resolution order): C->A->B
+#Thus, A will be printed.
+#diamond problem
+'''
+     A
+    / \\
+    B  C
+    \\ /
+      D'''
+#This means that B and C are derived from A, and D is derived by B and C
+class A:
+    def hello(self):
+        print("Hello from A")
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+class D(B, C):
+    pass
+
+d = D()
+d.hello()#Which A will be called?
+#MRO: D->B->C->A, So A will be called only once.
+class A:
+    def __init__(self):
+        print("A")
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("B")
+
+class C(A):
+    def __init__(self):
+        super().__init__()
+        print("C")
+
+class D(B, C):
+    def __init__(self):
+        super().__init__()
+        print("D")
+d = D()
+#What is the output?
+#Actually, in python, super means "call next class in MRO"
+#SO the output will be the reverse of MRO.
+
